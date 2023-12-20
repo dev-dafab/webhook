6bbd3b347294590f95795490ad5df590e98386ad
Fabrice Dufils Siyapdje <dev.dafab@gmail.com>
2023-12-20 10:51:31 +0100

    minor changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,48 +1,20 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-require('dotenv').config()
+import express from "express";
+require("dotenv").config();
 
 const router = express.Router();
- 
-const {
-SECRET_EMAIL_SERVER_HOST,
-SECRET_EMAIL_SERVER_PORT,
-SECRET_EMAIL_SERVER_SECURE,
-SECRET_EMAIL_SERVER_USERNAME,
-SECRET_EMAIL_SERVER_PASSWORD
-} = process.env;
 
-type BodyType = {
-  from: string;
-  to: string | string[];
-  subject: string;
-  content?: string;
-  html?: string;
-  attachments?: unknow[];
-}
+router.post<any, any>("/", async (req, res) => {
+  const body = req.body;
+  console.log("=====>>", body);
 
-router.post<BodyType, {message: string}>('/', async (req, res) => {
-  const {from ,to, subject, content = '', html = '', attachments = []} = req.body as BodyType;
+  res.json({ message: "OK" });
+});
 
-	const transporter = nodemailer.createTransport({
-		host: SECRET_EMAIL_SERVER_HOST,
-		port: Number(SECRET_EMAIL_SERVER_PORT),
-		secure: Boolean(SECRET_EMAIL_SERVER_SECURE),
-		auth: {
-			user: SECRET_EMAIL_SERVER_USERNAME,
-			pass: SECRET_EMAIL_SERVER_PASSWORD
-		}
-	});
+router.get<any, any>("/", async (req, res) => {
+  const body = req.body;
+  console.log("=====>>", body);
 
-  await transporter.sendMail({
-		from,
-		to,
-		attachments,
-		subject,
-		html,
-    text: content,
-	});
-  res.json({message: 'OK'});
+  res.json({ message: "OK" });
 });
 
 export default router;
